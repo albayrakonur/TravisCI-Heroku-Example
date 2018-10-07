e67f19cb4f51736e038326a6b952ebf7e3b2d7cb
Onur Albayrak <onuralbayrak@etu.edu.tr>
2018-10-08 00:24:48 +0300

    restore project to first state

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,83 +17,54 @@ import spark.template.mustache.MustacheTemplateEngine;
  *
  */
 public class App {
-  public static double calculate(ArrayList<Integer> array,int size_array, ArrayList<Integer> array2,int size_array2) {
-    double result = 0;
-    int summation_array = 0;
-    int summation_array2 = 0;
-    summation_array = getSumOfArrayList(array);
-    summation_array2 = getSumOfArrayList(array2);
-    int average_array = summation_array / size_array;
-    int average_array2 = summation_array2 / size_array2;
-    result = (average_array * 0.4) + (average_array2 * 0.6);
-    return result;
 
-  }
-
-  private static int getSumOfArrayList(ArrayList<Integer> array) {
-    int sum = 0;
-    for(int num : array) {
-      sum += num;
+  public static boolean search(ArrayList<Integer> array, int e) {
+    System.out.println("inside search");
+    if (array == null) return false;
+    for (int elt : array) {
+      if (elt == e) return true;
     }
-    return sum;
-
+    return false;
   }
 
   public static void main(String[] args) {
-      port(getHerokuAssignedPort());
 
-      get("/", (req, res) -> "Hello, World");
-
-      post("/compute", (req, res) -> {
-        //System.out.println(req.queryParams("input1"));
-        //System.out.println(req.queryParams("input2"));
-
-        String input1 = req.queryParams("input1");
-        java.util.Scanner sc1 = new java.util.Scanner(input1);
-        sc1.useDelimiter("[;\r\n]+");
-        java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-        while (sc1.hasNext())
-        {
-          int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-          inputList.add(value);
-        }
-        System.out.println(inputList);
-
-
-        String input2 = req.queryParams("input2");
-        java.util.Scanner sc2 = new java.util.Scanner(input2);
-        sc2.useDelimiter("[;\r\n]+");
-        java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
-        while (sc2.hasNext())
-        {
-          int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
-          inputList2.add(value);
-        }
-        System.out.println(inputList);
-
-        double result = App.calculate(inputList,inputList.size(), inputList2,inputList2.size());
-
-        Map map = new HashMap();
-        map.put("result", result);
-        return new ModelAndView(map, "compute.mustache");
-      }, new MustacheTemplateEngine());
-
-
-      get("/compute",
-          (rq, rs) -> {
-            Map map = new HashMap();
-            map.put("result", "not computed yet!");
-            return new ModelAndView(map, "compute.mustache");
-          },
-          new MustacheTemplateEngine());
+    port(getHerokuAssignedPort());
+    get("/", (req, res) -> "Hello, World");
+    post("/compute", (req, res) -> {
+    //System.out.println(req.queryParams("input1"));
+    //System.out.println(req.queryParams("input2"));
+    String input1 = req.queryParams("input1");
+    java.util.Scanner sc1 = new java.util.Scanner(input1);
+    sc1.useDelimiter("[;\r\n]+");
+    java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+    while (sc1.hasNext())
+    {
+      int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+      inputList.add(value);
+    }
+    System.out.println(inputList);
+    String input2 = req.queryParams("input2").replaceAll("\\s","");
+    int input2AsInt = Integer.parseInt(input2);
+    boolean result = App.search(inputList, input2AsInt);
+    Map map = new HashMap();
+    map.put("result", result);
+    return new ModelAndView(map, "compute.mustache");
+    }, new MustacheTemplateEngine());
+    get("/compute",
+        (rq, rs) -> {
+          Map map = new HashMap();
+          map.put("result", "not computed yet!");
+          return new ModelAndView(map, "compute.mustache");
+        },
+        new MustacheTemplateEngine());
   }
-
   static int getHerokuAssignedPort() {
-      ProcessBuilder processBuilder = new ProcessBuilder();
-      if (processBuilder.environment().get("PORT") != null) {
-          return Integer.parseInt(processBuilder.environment().get("PORT"));
-      }
-      return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 4567;
   }
 }
 
